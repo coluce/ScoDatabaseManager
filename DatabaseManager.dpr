17a776b621bf2af7de972552f5a665ec90b9d141
Ricardo Santos Coluce <ricardo.coluce@fagrontech.com.br>
2020-12-10 20:08:07 -0300

    .

--- DatabaseManager.dpr
+++ DatabaseManager.dpr
@@ -2,37 +2,41 @@ program DatabaseManager;
 
 uses
   Vcl.Forms,
-  View.Main in 'View\View.Main.pas' {ViewMain},
-  Controller.Imp.Main in 'Controller\Controller.Imp.Main.pas',
-  Model.Imp.Connection.SQLite in 'Model\Model.Imp.Connection.SQLite.pas',
+
   Model.Interfaces in 'Model\Model.Interfaces.pas',
   Model.Factory in 'Model\Model.Factory.pas',
-  Model.Imp.Table in 'Model\Model.Imp.Table.pas',
-  Model.Imp.Script in 'Model\Model.Imp.Script.pas',
   Model.Types in 'Model\Model.Types.pas',
-  Model.Imp.Updater in 'Model\Model.Imp.Updater.pas',
-  View.Query in 'View\View.Query.pas' {ViewQuery},
-  Controller.Imp.Query in 'Controller\Controller.Imp.Query.pas',
-  Controller.Interfaces in 'Controller\Controller.Interfaces.pas',
-  Controller.Factory in 'Controller\Controller.Factory.pas',
-  Model.Imp.Connection.Firebird in 'Model\Model.Imp.Connection.Firebird.pas',
-  View.Layout in 'View\View.Layout.pas' {ViewLayout},
-  Controller.Imp.Layout in 'Controller\Controller.Imp.Layout.pas',
-  View.Ini in 'View\View.Ini.pas' {ViewIni},
-  Controller.Imp.Ini in 'Controller\Controller.Imp.Ini.pas',
-  Controller.Imp.Param in 'Controller\Controller.Imp.Param.pas',
+
+  Model.Imp.Connection.SQLite in 'Model\Imp\Model.Imp.Connection.SQLite.pas',
+  Model.Imp.Connection.Firebird in 'Model\Imp\Model.Imp.Connection.Firebird.pas',
+  Model.Imp.Table in 'Model\Imp\Model.Imp.Table.pas',
+  Model.Imp.Script in 'Model\Imp\Model.Imp.Script.pas',
+  Model.Imp.Updater in 'Model\Imp\Model.Imp.Updater.pas',
+  Model.Imp.Firebird.Functions in 'Model\Imp\Model.Imp.Firebird.Functions.pas',
+  Model.Imp.Database.Backup in 'Model\Imp\Model.Imp.Database.Backup.pas',
+
+  View.Main in 'View\View.Main.pas' {ViewMain},
   View.Default in 'View\View.Default.pas' {ViewDefault},
-  Controller.Imp.Window in 'Controller\Controller.Imp.Window.pas',
+  View.Query in 'View\View.Query.pas' {ViewQuery},
+  View.Layout in 'View\View.Layout.pas' {ViewLayout},
+  View.Ini in 'View\View.Ini.pas' {ViewIni},
   View.Server in 'View\View.Server.pas' {ViewServer},
   View.Database.Register in 'View\View.Database.Register.pas' {ViewRegisterDatabase},
-  Model.Imp.Database.Backup in 'Model\Model.Imp.Database.Backup.pas',
   View.Backup.Manager in 'View\View.Backup.Manager.pas' {ViewBackupManager},
-  Controller.Imp.Backup.Manager in 'Controller\Controller.Imp.Backup.Manager.pas',
-  Controller.Imp.Param.Manager in 'Controller\Controller.Imp.Param.Manager.pas',
   View.Param.Manager in 'View\View.Param.Manager.pas' {ViewParamManager},
   View.Wait in 'View\View.Wait.pas' {ViewWait},
   View.Query.Param in 'View\View.Query.Param.pas' {ViewQueryParam},
-  Model.Imp.Firebird.Functions in 'Model\Model.Imp.Firebird.Functions.pas';
+
+  Controller.Interfaces in 'Controller\Controller.Interfaces.pas',
+  Controller.Factory in 'Controller\Controller.Factory.pas',
+  Controller.Imp.Query in 'Controller\Imp\Controller.Imp.Query.pas',
+  Controller.Imp.Layout in 'Controller\Imp\Controller.Imp.Layout.pas',
+  Controller.Imp.Ini in 'Controller\Imp\Controller.Imp.Ini.pas',
+  Controller.Imp.Param in 'Controller\Imp\Controller.Imp.Param.pas',
+  Controller.Imp.Window in 'Controller\Imp\Controller.Imp.Window.pas',
+  Controller.Imp.Backup.Manager in 'Controller\Imp\Controller.Imp.Backup.Manager.pas',
+  Controller.Imp.Param.Manager in 'Controller\Imp\Controller.Imp.Param.Manager.pas',
+  Controller.Imp.Main in 'Controller\Imp\Controller.Imp.Main.pas';
 
 {$R *.res}
 
--- Model/Imp/Model.Imp.Firebird.Functions.pas
+++ Model/Imp/Model.Imp.Firebird.Functions.pas
@@ -26,7 +26,7 @@ end;
 function TModelFirebirdFunctions.GetExecutionPlan(const ASQL: string): string;
 var
   vHandle: THandle;
-  isc_dsql_sql_info: FARPROC;
+  //isc_dsql_sql_info: TDLLFunc;
 begin
   Result := EmptyStr;
   try
@@ -36,13 +36,13 @@ begin
     if vHandle <> 0 then
     begin
       // get function address
-      @isc_dsql_sql_info := getProcAddress(vHandle, 'isc_dsql_sql_info');
-
-      // if function address exists
-      if Addr( isc_dsql_sql_info ) <> nil then
-      begin
-        Result := isc_dsql_sql_info(ASQL);
-      end;
+//      @isc_dsql_sql_info := getProcAddress(vHandle, 'isc_dsql_sql_info');
+//
+//      // if function address exists
+//      if Addr( isc_dsql_sql_info ) <> nil then
+//      begin
+//        Result := isc_dsql_sql_info(ASQL);
+//      end;
 
     end;
 
