091870f89fb4fc36db633e926757f2a971bc6c80
Ricardo Santos Coluce <ricardo.coluce@fagrontech.com.br>
2020-12-10 19:50:27 -0300

    .

--- DatabaseManager.dpr
+++ DatabaseManager.dpr
@@ -2,7 +2,7 @@ program DatabaseManager;
 
 uses
   Vcl.Forms,
-  View.Main in 'View\View.Main.pas' {ViewMain} ,
+  View.Main in 'View\View.Main.pas' {ViewMain},
   Controller.Imp.Main in 'Controller\Controller.Imp.Main.pas',
   Model.Imp.Connection.SQLite in 'Model\Model.Imp.Connection.SQLite.pas',
   Model.Interfaces in 'Model\Model.Interfaces.pas',
@@ -11,29 +11,28 @@ uses
   Model.Imp.Script in 'Model\Model.Imp.Script.pas',
   Model.Types in 'Model\Model.Types.pas',
   Model.Imp.Updater in 'Model\Model.Imp.Updater.pas',
-  View.Query in 'View\View.Query.pas' {ViewQuery} ,
+  View.Query in 'View\View.Query.pas' {ViewQuery},
   Controller.Imp.Query in 'Controller\Controller.Imp.Query.pas',
   Controller.Interfaces in 'Controller\Controller.Interfaces.pas',
   Controller.Factory in 'Controller\Controller.Factory.pas',
   Model.Imp.Connection.Firebird in 'Model\Model.Imp.Connection.Firebird.pas',
-  View.Layout in 'View\View.Layout.pas' {ViewLayout} ,
+  View.Layout in 'View\View.Layout.pas' {ViewLayout},
   Controller.Imp.Layout in 'Controller\Controller.Imp.Layout.pas',
-  View.Ini in 'View\View.Ini.pas' {ViewIni} ,
+  View.Ini in 'View\View.Ini.pas' {ViewIni},
   Controller.Imp.Ini in 'Controller\Controller.Imp.Ini.pas',
   Controller.Imp.Param in 'Controller\Controller.Imp.Param.pas',
-  View.Default in 'View\View.Default.pas' {ViewDefault} ,
+  View.Default in 'View\View.Default.pas' {ViewDefault},
   Controller.Imp.Window in 'Controller\Controller.Imp.Window.pas',
-  View.Server in 'View\View.Server.pas' {ViewServer} ,
-  View.Database.Register
-    in 'View\View.Database.Register.pas' {ViewRegisterDatabase} ,
+  View.Server in 'View\View.Server.pas' {ViewServer},
+  View.Database.Register in 'View\View.Database.Register.pas' {ViewRegisterDatabase},
   Model.Imp.Database.Backup in 'Model\Model.Imp.Database.Backup.pas',
-  View.Backup.Manager in 'View\View.Backup.Manager.pas' {ViewBackupManager} ,
-  Controller.Imp.Backup.Manager
-    in 'Controller\Controller.Imp.Backup.Manager.pas',
+  View.Backup.Manager in 'View\View.Backup.Manager.pas' {ViewBackupManager},
+  Controller.Imp.Backup.Manager in 'Controller\Controller.Imp.Backup.Manager.pas',
   Controller.Imp.Param.Manager in 'Controller\Controller.Imp.Param.Manager.pas',
-  View.Param.Manager in 'View\View.Param.Manager.pas' {ViewParamManager} ,
-  View.Wait in 'View\View.Wait.pas' {ViewWait} ,
-  View.Query.Param in 'View\View.Query.Param.pas' {ViewQueryParam};
+  View.Param.Manager in 'View\View.Param.Manager.pas' {ViewParamManager},
+  View.Wait in 'View\View.Wait.pas' {ViewWait},
+  View.Query.Param in 'View\View.Query.Param.pas' {ViewQueryParam},
+  Model.Imp.Firebird.Functions in 'Model\Model.Imp.Firebird.Functions.pas';
 
 {$R *.res}
 
---
+++ Model/Model.Imp.Firebird.Functions.pas
@@ -0,0 +1,54 @@
+unit Model.Imp.Firebird.Functions;
+
+interface
+
+type
+  TModelFirebirdFunctions = class
+  private
+    FDLLPath: string;
+  public
+    constructor Create(const ADLLPath: string);
+    function GetExecutionPlan(const ASQL: string): string;
+  end;
+
+implementation
+
+uses
+  Windows, System.SysUtils;
+
+{ TModelFirebirdFunctions }
+
+constructor TModelFirebirdFunctions.Create(const ADLLPath: string);
+begin
+  FDLLPath := ADLLPath;
+end;
+
+function TModelFirebirdFunctions.GetExecutionPlan(const ASQL: string): string;
+var
+  vHandle: THandle;
+  isc_dsql_sql_info: FARPROC;
+begin
+  Result := EmptyStr;
+  try
+    // load dll in dinamic type(mode)
+    vHandle := LoadLibrary(PWideChar(FDLLPath));
+
+    if vHandle <> 0 then
+    begin
+      // get function address
+      @isc_dsql_sql_info := getProcAddress(vHandle, 'isc_dsql_sql_info');
+
+      // if function address exists
+      if Addr( isc_dsql_sql_info ) <> nil then
+      begin
+        Result := isc_dsql_sql_info(ASQL);
+      end;
+
+    end;
+
+  finally
+    freeLibrary (vHandle);
+  end;
+end;
+
+end.
